28263d03e51e96518a456cc03cbd96f1bd6d7a46
linooohon <linooohon@gmail.com>
2022-02-17 10:09:32 +0800

    feat: add script

---
+++ scheduler_exec.sh
@@ -0,0 +1,3 @@
+cd /home/linooohongcptest/airflowtest/
+source ./.venv/bin/activate
+airflow scheduler
---
+++ webserver_exec.sh
@@ -0,0 +1,3 @@
+cd /home/linooohongcptest/airflowtest/
+source ./.venv/bin/activate
+airflow webserver -p 8080
